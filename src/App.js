9d76146d9c488246e138e3d9343cf9e3e091e4bf
haybuka <paschal.nduleo@gmail.com>
2023-10-20 00:24:33 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import { lazy } from 'react';
+import Home from './pages';
+import Contact from './pages/contact';
+import Services from './pages/services';
+import NavBar from './layout'
+import About from './pages/about';
 
-const Home = lazy(() => import("./pages"));
-const NavBar = lazy(() => import("./layout"));
-const Contact = lazy(() => import("./pages/contact"));
+// const Home = lazy(() => import("./pages"));
+// const NavBar = lazy(() => import("./layout"));
+// const Contact = lazy(() => import("./pages/contact"));
+// const Services = lazy(() => import("./pages/services"));
+// const About = lazy(() => import("./pages/about"));
 
 function App() {
   return (
@@ -15,6 +22,8 @@ function App() {
         <Route element={<NavBar />}>
           <Route index element={<Home />} />
           <Route path='contact' element={<Contact />} />
+          <Route path='services' element={<Services />} />
+          <Route path='about' element={<About />} />
         </Route>
       </Routes>
     </>
